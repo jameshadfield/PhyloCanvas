d33f2e08ec4bb5ad7ca1c0f60f4ee873dd5d4532
Richard Goater <richard.goater27@gmail.com>
2015-12-13 17:43:43 +0000

    Attempting to calculate node shapes by area

--- src/nodeRenderers.js
+++ src/nodeRenderers.js
@@ -28,15 +28,11 @@ export default {
   },
 
   square(canvas, radius, style) {
-    const lengthOfSide = radius * Math.sqrt(2);
-    const startX = radius - lengthOfSide / 2;
+    const areaOfShape = Math.PI * Math.pow(radius, 2);
 
-    // connector
-    canvas.beginPath();
-    canvas.moveTo(0, 0);
-    canvas.lineTo(startX, 0);
-    canvas.stroke();
-    canvas.closePath();
+    // square
+    const lengthOfSide = Math.sqrt(areaOfShape);
+    const startX = radius - lengthOfSide / 2;
 
     canvas.beginPath();
     canvas.moveTo(startX, 0);
@@ -53,9 +49,9 @@ export default {
   star(canvas, radius, style) {
     const cx = radius;
     const cy = 0;
-    const spikes = 8;
-    const outerRadius = radius;
-    const innerRadius = radius * 0.5;
+    const spikes = 5;
+    const outerRadius = radius + radius * (1 / spikes);
+    const innerRadius = outerRadius / 2;
     const step = Math.PI / spikes;
 
     let rot = Math.PI / 2 * 3;
@@ -80,16 +76,21 @@ export default {
   },
 
   triangle(canvas, radius, style) {
-    const lengthOfSide = (2 * radius) * Math.cos(30 * Math.PI / 180);
+    const areaOfShape = Math.PI * Math.pow(radius, 2);
+
+    // triangle
+    const lengthOfSide = Math.sqrt(
+      areaOfShape / (Math.sqrt(3) / 4)
+    );
+    const height = (Math.sqrt(3) / 2) * lengthOfSide;
+    const midpoint = (1 / Math.sqrt(3)) * (lengthOfSide / 2);
 
     canvas.beginPath();
-    canvas.moveTo(0, 0);
-    canvas.rotate(30 * Math.PI / 180);
-    canvas.lineTo(lengthOfSide, 0);
-    canvas.rotate(-60 * Math.PI / 180);
-    canvas.lineTo(lengthOfSide, 0);
-    canvas.rotate(30 * Math.PI / 180);
-    canvas.lineTo(0, 0);
+    canvas.moveTo(radius, midpoint);
+    canvas.lineTo(radius + lengthOfSide / 2, midpoint);
+    canvas.lineTo(radius, -(height - midpoint));
+    canvas.lineTo(radius - lengthOfSide / 2, midpoint);
+    canvas.lineTo(radius, midpoint);
     canvas.closePath();
 
     commitPath(canvas, style);
